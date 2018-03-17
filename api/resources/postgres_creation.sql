73399c4572beb8c46323cb6300321ca08bbfc824
wuzzapcom <wuzzapcom@gmail.com>
2018-03-17 23:42:24 +0300

    Обновлена структура бд. Не окончена настройка пользователя.

--- api/resources/postgres_creation.sql
+++ api/resources/postgres_creation.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE bibtex_literature;
 
-DROP SCHEMA schema;
+DROP SCHEMA schema CASCADE;
 
 CREATE SCHEMA schema;
 
@@ -12,39 +12,49 @@ CREATE TABLE schema.textbook(
   publisher VARCHAR(100) NOT NULL,
   year INTEGER NOT NULL,
   isbn VARCHAR(20) NOT NULL,
-  url VARCHAR(100) NOT NULL
+  url VARCHAR(100) NOT NULL,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL
 );
 
 CREATE TABLE schema.department(
   department_id SERIAL PRIMARY KEY,
-  title VARCHAR(40) NOT NULL
+  title VARCHAR(40) UNIQUE NOT NULL,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL
 );
 
 CREATE TABLE schema.lecturer(
   lecturer_id SERIAL PRIMARY KEY,
   name VARCHAR(40) NOT NULL,
   date_of_birth date NOT NULL,
-  department_id SERIAL REFERENCES schema.department
+  department_id SERIAL REFERENCES schema.department,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL,
+  UNIQUE (name, date_of_birth)
 );
 
 CREATE TABLE schema.course(
   id SERIAL PRIMARY KEY,
   title VARCHAR(40) NOT NULL,
   lecturer_id SERIAL REFERENCES schema.lecturer,
-  department_id SERIAL REFERENCES schema.department
+  department_id SERIAL REFERENCES schema.department,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL
+);
+
+CREATE TABLE schema.literature_lists(
+  id SERIAL PRIMARY KEY,
+  year INTEGER,
+  course SERIAL REFERENCES schema.course,
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL
 );
 
 CREATE TABLE schema.literature(
   textbook_id INTEGER REFERENCES schema.textbook,
   literature_list_id SERIAL REFERENCES schema.literature_lists,
-  PRIMARY KEY (textbook_id, literature_list_id)
+  PRIMARY KEY (textbook_id, literature_list_id),
+  is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  timestamp INTEGER NOT NULL
 );
-
-CREATE TABLE schema.literature_lists(
-  id SERIAL PRIMARY KEY,
-  year INTEGER
-);
-
-INSERT INTO schema.lecturer(name, date_of_birth, department_id) VALUES ('Trishin', '12.02.1997', 1);
-INSERT INTO schema.department(title) VALUES ('UI9');
-INSERT INTO schema.course(title, lecturer_id, department_id) VALUES ('Compilers', 1, 1);
