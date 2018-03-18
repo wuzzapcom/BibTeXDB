f6214a65825d2ccf8c7aa17547a3e885fea39099
wuzzapcom <wuzzapcom@gmail.com>
2018-03-18 22:50:01 +0300

    Добавлено форматирование вывода для новый структур

--- api/src/common/literature.go
+++ api/src/common/literature.go
@@ -1,9 +1,16 @@
 package common
 
+import "encoding/json"
+
 // Literature ..
 type Literature struct {
 	BookIdent       string
 	Year            int
 	CourseTitle     string
 	DepartmentTitle string
+}
+
+func (literature Literature) String() string {
+	data, _ := json.MarshalIndent(literature, "", "\t")
+	return string(data)
 }
--- api/src/common/literature_list.go
+++ api/src/common/literature_list.go
@@ -1,9 +1,16 @@
 package common
 
+import "encoding/json"
+
 // LiteratureList ..
 type LiteratureList struct {
 	ID              int `json:"omitempty"`
 	Year            int
 	DepartmentTitle string
 	CourseTitle     string
+}
+
+func (list LiteratureList) String() string {
+	data, _ := json.MarshalIndent(list, "", "\t")
+	return string(data)
 }
