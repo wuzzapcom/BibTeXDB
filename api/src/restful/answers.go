2a51ed4d7bbe3f61f3c4b377b636087a8d6bfa9c
wuzzapcom <wuzzapcom@gmail.com>
2018-04-01 16:20:27 +0300

    Добавлены String методы к некоторым структурам

--- api/src/restful/answers.go
+++ api/src/restful/answers.go
@@ -24,9 +24,25 @@ type Courses struct {
 	CoursesList []common.Course `json:"courses"`
 }
 
+func (courses Courses) String() (result string) {
+	for _, course := range courses.CoursesList {
+		result += course.String()
+		result += "\n"
+	}
+	return
+}
+
 // Departments ..
 type Departments struct {
 	DepartmentList []common.Department `json:"departments"`
+}
+
+func (departments Departments) String() (result string) {
+	for _, department := range departments.DepartmentList {
+		result += department.String()
+		result += "\n"
+	}
+	return
 }
 
 // Lecturers ..
@@ -34,9 +50,25 @@ type Lecturers struct {
 	LecturerList []common.Lecturer `json:"lecturers"`
 }
 
+func (lecturers Lecturers) String() (result string) {
+	for _, lecturer := range lecturers.LecturerList {
+		result += lecturer.String()
+		result += "\n"
+	}
+	return
+}
+
 // LiteratureLists ..
 type LiteratureLists struct {
 	StoredLists []common.LiteratureList `json:"lists"`
+}
+
+func (literatureLists LiteratureLists) String() (result string) {
+	for _, literatureList := range literatureLists.StoredLists {
+		result += literatureList.String()
+		result += "\n"
+	}
+	return
 }
 
 // Literature ..
@@ -44,11 +76,26 @@ type Literature struct {
 	StoredLiterature []common.Literature `json:"literature"`
 }
 
+func (literature Literature) String() (result string) {
+	for _, l := range literature.StoredLiterature {
+		result += l.String()
+		result += "\n"
+	}
+	return
+}
+
 //Books ..
 type Books struct {
 	StoredBooks common.Items `json:"storedBooks"`
 }
 
+func (books Books) String() (result string) {
+	for _, book := range books.StoredBooks {
+		result += book.String()
+		result += "\n"
+	}
+	return
+}
 func (search Search) String() string {
 
 	return search.Results.String()
